d5d331437ad1183095ab41813a110784ca809225
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-09-27 22:44:31 +0200

    First working prototype

--- build-project.js
+++ build-project.js
@@ -1,7 +1,7 @@
 // Script that builds a DOGE project
 
 const chalk = require("chalk");
-const fs = require("fs");
+const fs = require("fs-extra");
 const path = require("path");
 
 const projectDir = process.argv[2];
@@ -26,4 +26,72 @@ if (!fs.existsSync(outDir)) {
   fs.mkdirSync(outDir);
 }
 
-console.log(chalk.greenBright(`Finished building '${project.project.name}'`));
+let templateHtml = fs.readFileSync(
+  path.join(__dirname, "template/index.html"),
+  "utf8"
+);
+
+const imgLoads = [];
+project.sprites.forEach(sprite => {
+  imgLoads.push(`this.load.image("${sprite.name}", "assets/${sprite.image}");`);
+});
+
+const objectDefs = [];
+project.objects.forEach(object => {
+  objectDefs.push(
+    `function ${
+      object.name
+    }(self){this.update=function(){${object.events.update
+      .split("\n")
+      .join("")}}}`
+  );
+});
+
+const instanceCreates = [];
+project.objects.forEach(object => {
+  instanceCreates.push(
+    `instances.push(new ${object.name}(this.add.image(0, 0, "${object.sprite}")));`
+  );
+});
+
+templateHtml = templateHtml
+  .split(`"$DOGE_LOAD_IMAGE";`)
+  .join(imgLoads.join("\n"));
+
+console.log(`Wrote ${imgLoads.length} image load(s)`);
+
+templateHtml = templateHtml
+  .split(`("$DOGE_OBJECT_DEFINITIONS");`)
+  .join(objectDefs.join("\n"));
+
+console.log(`Wrote ${objectDefs.length} object definition(s)`);
+
+templateHtml = templateHtml
+  .split(`"$DOGE_CREATE_INSTANCES";`)
+  .join(instanceCreates.join("\n"));
+
+console.log(`Wrote ${instanceCreates.length} instance creator(s)`);
+
+fs.writeFileSync(path.join(outDir, "index.html"), templateHtml);
+console.log(chalk.green("Out: index.html"));
+
+fs.copyFileSync(
+  path.join(__dirname, "phaser/phaser.min.js"),
+  path.join(outDir, "phaser.min.js")
+);
+
+console.log(chalk.green("Out: phaser.min.js"));
+
+if (!fs.existsSync(path.join(outDir, "assets"))) {
+  fs.mkdirSync(path.join(outDir, "assets"));
+}
+
+//Copy sprite assets
+project.sprites.forEach(sprite => {
+  fs.copyFileSync(
+    path.join(projectDir, `sprites/assets/${sprite.image}`),
+    path.join(outDir, `assets/${sprite.image}`)
+  );
+});
+
+console.log(chalk.green("Out: assets"));
